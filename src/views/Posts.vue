f26fe2e2ed4096505d29931c3d31ca7d87cec6fc
Anzalla Dzikri Dhamara <anzalladd@gmail.com>
2018-12-09 13:59:26 -0800

    build client and server

---
+++ src/views/Posts.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="about">
+    <h1>This is an Post page</h1>
+    <div v-for="post in posts">
+      <span><b>{{post.title}}</b></span>
+      <br>
+      <span>{{post.description}}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+import PostsService from '@/services/PostsService'
+  export default{
+    name: 'posts',
+    data(){
+      return{
+        posts:[]
+      }
+    },
+     mounted() {
+    this.getPosts();
+  },
+  methods: {
+    async getPosts () {
+      const response = await PostsService.fetchPosts()
+      this.posts = response.data
+    }
+  }
+  }
+ 
+
+</script>
+
+<style lang="scss" scoped>
+ .about > div{
+   margin-top: 50px;
+ }
+</style>
